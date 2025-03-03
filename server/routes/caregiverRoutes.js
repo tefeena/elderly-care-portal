a73d08c04b1b3924566007a2a220affdcf5e53a0
swethaa1004 <swethasaran1001@gmail.com>
2025-03-03 04:29:34 -0500

    Emergency page created

--- server/routes/caregiverRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getCaregivers } = require('../controllers/caregiverController');
-const router = express.Router();
-
-router.get('/all', getCaregivers);
-
-module.exports = router;
