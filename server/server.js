9503fd756abb12ce25995868f0120361d560627f
Tefeena <tefeenakjohn@gmail.com>
2025-04-15 20:46:05 -0400

    sever allowedOrigins

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://elderly-care-portal.vercel.app",
-  "https://elderly-care-portal-c44zhiehn-tefeenas-projects.vercel.app",
+  "https://elderly-care-portal-ncpghacf7-tefeenas-projects.vercel.app",
 ];
 
 const corsOptions = {
